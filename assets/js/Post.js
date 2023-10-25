72be45ec3335d4e1e825dc98f1de50683c2c3129
guillaume806 <guillaumebodart@outlook.fr>
2023-10-25 14:57:33 +0200

    first commit

---
+++ assets/js/Post.js
@@ -0,0 +1,37 @@
+// Fonction pour obtenir les informations requises d'un Pokémon
+async function getPokemonDetails(data, speciesData) {
+    const name = data.name;
+    const types = data.types.map(type => type.type.name).join(', ');
+    const abilities = data.abilities.map(ability => ability.ability.name).join(', ');
+    const weight = data.weight;
+    const height = data.height;
+    const dexNumber = data.id;
+    const frenchName = getFrenchName(speciesData.names);
+    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${dexNumber}.png`;
+
+    const pokemonDetails = `
+        <h2>${frenchName} (${name})</h2>
+        <img src="${imageUrl}" alt="${frenchName}">
+        <p>Types: ${types}</p>
+        <p>Capacités: ${abilities}</p>
+        <p>Poids: ${weight} kg</p>
+        <p>Taille: ${height} dm</p>
+        <p>Numéro de Pokédex National: ${dexNumber}</p>
+    `;
+
+    return pokemonDetails;
+}
+
+// ...
+
+
+// Fonction pour obtenir le nom en français depuis le tableau de noms
+function getFrenchName(names) {
+    for (const nameData of names) {
+        if (nameData.language.name === "fr") {
+            return nameData.name;
+        }
+    }
+    return "Nom français non disponible";
+}
+
---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('searchInput');
+    const searchButton = document.getElementById('searchButton');
+    const prevButton = document.getElementById('prevButton');
+    const nextButton = document.getElementById('nextButton');
+    const pokemonInfo = document.getElementById('pokemonInfo');
+    let currentPokemonId = 1;
+
+// ...
+
+    async function loadPokemonData(id) {
+        try {
+
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+            const data = await response.json();
+
+
+            const speciesResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+            const speciesData = await speciesResponse.json();
+
+
+            const pokemonDetails = await getPokemonDetails(data, speciesData);
+
+            pokemonInfo.innerHTML = pokemonDetails;
+        } catch (error) {
+            console.error('Erreur lors du chargement des données du Pokémon', error);
+        }
+    }
+
+// ...
+
+
+    loadPokemonData(currentPokemonId);
+
+    searchButton.addEventListener('click', function () {
+        const searchTerm = searchInput.value;
+        if (!isNaN(searchTerm) && searchTerm >= 1 && searchTerm <= 1027) {
+            currentPokemonId = parseInt(searchTerm);
+            loadPokemonData(currentPokemonId);
+        }
+    });
+
+    prevButton.addEventListener('click', function () {
+        if (currentPokemonId > 1) {
+            currentPokemonId--;
+            loadPokemonData(currentPokemonId);
+        }
+    });
+
+    nextButton.addEventListener('click', function () {
+        if (currentPokemonId < 1027) {
+            currentPokemonId++;
+            loadPokemonData(currentPokemonId);
+        }
+    });
+});
